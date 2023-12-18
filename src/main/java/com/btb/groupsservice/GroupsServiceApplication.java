64297ddc5782fe1835a5b4777cca41eb9ee8b28a
Edgar <edouton@uoc.edu>
2023-12-19 00:10:23 +0100

    Add mappers config and change ports

--- src/main/java/com/btb/groupsservice/GroupsServiceApplication.java
+++ src/main/java/com/btb/groupsservice/GroupsServiceApplication.java
@@ -1,8 +1,14 @@
 package com.btb.groupsservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.btb.groupsservice.persistence.mapper"})
 @SpringBootApplication
 public class GroupsServiceApplication {
 
--- src/main/java/com/btb/groupsservice/controller/CanalController.java
+++ src/main/java/com/btb/groupsservice/controller/CanalController.java
@@ -5,6 +5,7 @@ import com.btb.groupsservice.dto.UpdateCanalDTO;
 import com.btb.groupsservice.entity.Canal;
 import com.btb.groupsservice.exception.CanalException;
 import com.btb.groupsservice.exception.GroupException;
+import com.btb.groupsservice.exception.GroupMembershipException;
 import com.btb.groupsservice.service.CanalService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,7 @@ public class CanalController {
     }
 
     @PostMapping("/")
-    public void addCanal(@RequestBody AddCanalDTO addCanalDTO) throws GroupException {
+    public void addCanal(@RequestBody AddCanalDTO addCanalDTO) throws GroupException, GroupMembershipException {
         log.trace("POST /canals {}", addCanalDTO.getName());
 
         log.info("Event: Add canal: {}", addCanalDTO.getName());
@@ -33,7 +34,7 @@ public class CanalController {
     }
 
     @GetMapping("/group/{groupdId}")
-    public @ResponseBody List<Canal> getCanals(@PathVariable("groupId") Long groupId) throws GroupException {
+    public @ResponseBody List<Canal> getCanals(@PathVariable("groupId") Long groupId) throws GroupException, GroupMembershipException {
         log.trace("GET /canals/group/{}", groupId);
 
         log.info("Event: Get canals from group: {}", groupId);
@@ -41,7 +42,7 @@ public class CanalController {
     }
 
     @GetMapping("/{canalId}")
-    public Canal getCanal(@PathVariable("canalId") Long canalId) throws CanalException {
+    public Canal getCanal(@PathVariable("canalId") Long canalId) throws CanalException, GroupMembershipException {
         log.trace("GET /canals/{}", canalId);
 
         log.info("Event: Get canal: {}", canalId);
@@ -49,7 +50,7 @@ public class CanalController {
     }
 
     @PutMapping("/{canalId}")
-    public void updateCanal(@PathVariable("canalId") Long canalId, @RequestBody UpdateCanalDTO updateCanalDTO) throws CanalException {
+    public void updateCanal(@PathVariable("canalId") Long canalId, @RequestBody UpdateCanalDTO updateCanalDTO) throws CanalException, GroupMembershipException {
         log.trace("PUT /canals/{}", canalId);
 
         log.info("Event: Update canal: {}", canalId);
@@ -57,7 +58,7 @@ public class CanalController {
     }
 
     @DeleteMapping("/{canalId}")
-    public void deleteCanal(@PathVariable("canalId") Long canalId) throws CanalException {
+    public void deleteCanal(@PathVariable("canalId") Long canalId) throws CanalException, GroupMembershipException {
         log.trace("DELETE /canals/{}", canalId);
 
         log.info("Event: Delete canal: {}", canalId);
